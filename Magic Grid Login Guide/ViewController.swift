4567d778f21f1295c1730262d46fae0a78cf86e0
Viktor Varsano <Viktor_varsano@hotmail.com>
2021-01-20 16:40:57 +0200

    add gesture recognizer

--- Magic Grid Login Guide/ViewController.swift
+++ Magic Grid Login Guide/ViewController.swift
@@ -16,19 +16,26 @@ class ViewController: UIViewController {
         let numViewPerRow = 15
         let width = view.frame.width / CGFloat(numViewPerRow)
        
-        for j in 0...20{
+        for j in 0...30 {
             for i in 0...numViewPerRow {
-                let redView = UIView()
-                redView.backgroundColor = randomColor()
-                redView.frame = CGRect(x: CGFloat(i) * width, y: CGFloat(j) * width, width: width, height: width)
-                
-                view.addSubview(redView)
+                let cellView = UIView()
+                cellView.backgroundColor = randomColor()
+                cellView.frame = CGRect(x: CGFloat(i) * width, y: CGFloat(j) * width, width: width, height: width)
+                cellView.layer.borderWidth = 0.5
+                cellView.layer.borderColor = UIColor.black.cgColor
+                view.addSubview(cellView)
             }
         }
-      
-        
-    }
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
+
+        }
     
+        @objc func handlePan(gesture: UIPanGestureRecognizer) {
+            let location = gesture.location(in: view)
+            print(location)
+        }
+    
+        
     fileprivate func randomColor() -> UIColor {
         
         let red = CGFloat(drand48())
@@ -37,6 +44,10 @@ class ViewController: UIViewController {
 
         return UIColor(red: red, green: green, blue: blue, alpha: 1)
     }
-
+    
+    
+    
+    
 }
+
 
